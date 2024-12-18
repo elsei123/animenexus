c077965461d2808ac5bc5063665e0a85d51dad35
elsei123 <eltonseidi@gmail.com>
2024-12-18 04:30:25 +0000

    Add media URL configuration for development in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import home_page, post_detail, about_page, contact_page, post_list
 
 urlpatterns = [
-    path('', home_page, name='home'),
+    path('', post_list, name='post_list'),
     path('post/<int:post_id>/', post_detail, name='post_detail'),
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='contact'),
-    path('', post_list, name='post_list'),
 ]
+

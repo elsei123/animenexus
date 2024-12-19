8932f03ecfc40dc948f8dac1b6665f7ca8d26f57
elsei123 <eltonseidi@gmail.com>
2024-12-19 02:36:03 +0000

    Update settings.py

--- animenexus/settings.py
+++ animenexus/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default="django-insecure-f%zwu(e_il2x0r6=d963-3&sybjjby^*ov30$w95t27aetlyb*")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['8001-elsei123-animenexus-fnqjono20l.app.codeanywhere.com']
+ALLOWED_HOSTS = ['8001-elsei123-animenexus-fnqjono20l.app.codeanywhere.com', 
+'.herokuapp.com'
+]
 
 
 CSRF_TRUSTED_ORIGINS = [

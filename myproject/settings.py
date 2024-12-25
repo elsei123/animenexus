775430d2504d02efe5eb93618f64632375326929
elsei123 <eltonseidi@gmail.com>
2024-12-25 13:57:09 +0000

    apply database migrations

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
-from decouple import config 
-import dj_database_url 
+from decouple import config  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

e334f86f15f6b1380977bdc02295e557243ad05a
elsei123 <eltonseidi@gmail.com>
2024-12-26 21:36:48 +0000

    Add environment variable loading to wsgi.py using dotenv

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
+
+# Defina o BASE_DIR
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 

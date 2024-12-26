904b00b7a521815390f1ec4d765abc7a3ab8631f
elsei123 <eltonseidi@gmail.com>
2024-12-26 09:01:31 +0000

    Fix media handling on Heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -149,6 +149,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
+WHITENOISE_MANIFEST_STRICT = False
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 

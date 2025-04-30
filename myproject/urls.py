fb7d62721246240c0f5d87c22c449bdafd07cf64
elsei123 <eltonseidi@gmail.com>
2025-04-30 18:01:42 +0000

    Create custom login template with CSRF protection and base layout

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', blog_views.signup, name='signup'),
     path('', include('blog.urls')),
 ]
 

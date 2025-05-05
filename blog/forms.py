7b9881390a5a00e39331afa28750781fe602c3ea
elsei123 <eltonseidi@gmail.com>
2025-05-05 19:06:41 +0000

    PostForm with custom widgets for title and content fields

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Comment
+from .models import Comment, post 
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,23 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, label='Nome')
     email = forms.EmailField(label='Email')
     message = forms.CharField(widget=forms.Textarea, label='Mensagem')
+
+class PostForm(forms.ModelForm):
+    """
+    Form for creating and editing Posts.
+    """
+    class Meta:
+        model = Post
+        # Adjust these fields according to your Post model
+        fields = ['title', 'content', 'category', 'cover_image']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'placeholder': 'Post title',
+                'class': 'form-control'
+            }),
+            'content': forms.Textarea(attrs={
+                'rows': 5,
+                'placeholder': 'Post content',
+                'class': 'form-control'
+            }),
+        }

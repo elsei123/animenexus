e0d792faa1cf5f8aa55a68533c54bf6d00b5209d
elsei123 <eltonseidi@gmail.com>
2025-05-05 21:03:58 +0000

    Simplify CommentForm to only body field with label & textarea widget

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,13 @@ from .models import Comment, Post
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['name', 'body']
+        fields = ['body']
+        labels = {
+            'body': 'Comment',
+        }
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 4}),
+        }
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, label='Nome')

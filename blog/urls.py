0f2f99562a5ba4ff17f45cd4acfba3f8265d89ec
elsei123 <eltonseidi@gmail.com>
2025-05-18 15:16:02 +0000

    Expose login/logout views in Animenexus namespace and update template URL references

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 app_name = 'Animenexus'
 
@@ -23,4 +25,11 @@ urlpatterns = [
     # Static pages
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+
+    path('login/',
+         auth_views.LoginView.as_view(template_name='registration/login.html'),
+         name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(next_page='Animenexus:post_list'),
+         name='logout'),
 ]

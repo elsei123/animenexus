0e027f97da0f07b6eb5554f0168ba1e1d8300383
Elton Seidi <eltonseidi@MacBook-Air-de-Elton.local>
2025-08-13 20:39:19 +0100

    routes: use /signup/ as canonical path; remove duplicate /accounts/signup/

--- myproject/urls.py
+++ myproject/urls.py
@@ -25,7 +25,6 @@ import blog.views as blog_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/signup/", blog_views.signup, name="signup"),
     path("", include("blog.urls")),
 ]
 

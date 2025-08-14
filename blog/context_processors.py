4ce63f46f38b641a34eb0a6624b64bb917060de2
Elton Seidi <eltonseidi@MacBook-Air-de-Elton.local>
2025-08-14 02:43:57 +0100

    Optimize context processor: renamed function and added caching for category list

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,5 +1,10 @@
+from django.core.cache import cache
 from .models import Category
 
+def all_categories_context(request):
+    categories = cache.get("all_categories")
+    if categories is None:
+        categories = list(Category.objects.order_by("name"))
+        cache.set("all_categories", categories, 300)  # guarda por 5 minutos
+    return {"all_categories": categories}
 
-def categories(request):
-    return {"all_categories": Category.objects.order_by("name")}

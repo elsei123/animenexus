1560785d90d52775c0ba8109eb50b21fb3cafb2c
elsei123 elsei <eltonseidi@gmail.com>
2025-08-29 23:19:46 +0000

    fix: import user_logged_out signal to prevent NameError in Heroku

--- blog/signals.py
+++ blog/signals.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.signals import user_logged_in
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from django.contrib import messages
 
